7117632ffe0826d4fc2da2e6edcd2dc712099328
Ilder Gutierrez <125604310+ildergutierrez@users.noreply.github.com>
2026-03-20 01:37:03 -0500

    E

--- js/accesoadmin.js
+++ js/accesoadmin.js
@@ -1,5 +1,5 @@
 document.addEventListener("keydown", (e) => {
-  if (e.ctrlKey && e.shiftKey && e.key === "E") {
+  if (e.ctrlKey && e.shiftKey && e.key === "E" || e.key === "e") {// Ctrl + Shift + E pero la letra E puede ser minuscula
     //si esta en la carperpeta raiz al oprimir la tecla se muestar la rura admin/index.html
     if (window.location.pathname === "/") {
       window.location.href = "/admin/index.html";
